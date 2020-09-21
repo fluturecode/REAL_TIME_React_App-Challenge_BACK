cb9e0c5161743e134bc554ebb1ff20d7ec4c73ef
Elizabeth Eidelson <fluturecode@gmail.com>
2020-09-21 16:52:49 -0400

    connected to mongo

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoosse";
+import mongoose from "mongoose";
 import Pusher from "pusher";
+import postModel from "./postModel.js";
 
 // app config
 const app = express();
@@ -11,8 +13,41 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
+// DB config
+mongoose.connect(process.env.MONGO_URL, {
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+	console.log("DB Connected");
+});
+
 // API routes
 app.get("/", (req, res) => res.status(200).send("Hello world"));
+
+app.post("/upload", (req, res) => {
+	const body = req.body;
+
+	postModel.create(body, (err, data) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(201).send(data);
+		}
+	});
+});
+
+app.get("/sync", (req, res) => {
+	postModel.find((err, data) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(200).send(data);
+		}
+	});
+});
 
 // listen
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
